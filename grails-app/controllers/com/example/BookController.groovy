f8379502293bce2ef21eb5ba520adaec7cce8640
yu.asano <asano.1105@gmail.com>
2014-04-17 22:59:18 +0900

    動的Scaffoldingを有効にする為のプロパティを設定

--- grails-app/controllers/com/example/BookController.groovy
+++ grails-app/controllers/com/example/BookController.groovy
@@ -8,6 +8,8 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class BookController {
 
+    static scaffold = true
+
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
